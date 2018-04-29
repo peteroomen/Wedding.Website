f4225c0b23b8615767cb102268272b2bec3596b3
Peter Oomen <petertheoomen@gmail.com>
2018-04-29 19:26:24 +1200

    Added the pages and begun the header bar

---
+++ app.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START app]
+const express = require('express');
+const app = express();
+
+// Set the view engine and configure
+app.set('view engine', 'pug');
+
+// Setup the static files
+app.use(express.static('static'));
+
+// Setup routes
+app.get('/', (req, res) => {
+	res.render('home', { page: 'home' });
+});
+
+// Setup routes
+app.get('/info', (req, res) => {
+	res.render('info', { page: 'info' });
+});
+
+app.get('/rsvp', (req, res) => {
+	res.render('rsvp', { page: 'rsvp' });
+});
+
+app.get('/registry', (req, res) => {
+	res.render('registry', { page: 'registry' });
+});
+
+app.get('/photos', (req, res) => {
+	res.render('photos', { page: 'photos' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+	console.log('Press Ctrl+C to quit.');
+});
+// [END app]
---
+++ static/js/header-bar.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var header = document.getElementById('header-bar');
+	var content = document.getElementById('content');
+	content.addEventListener('scroll', function() {
+		var scrollTop = content.scrollTop;
+		if (scrollTop > 48) {
+			header.style.top = '-108px';
+			content.style.top = '48px';
+		} else {
+			header.style.top = '0';
+			content.style.top = '156px';
+		}
+	});
+});
