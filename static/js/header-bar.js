9bb3ce2958de504c85eaff9006a1921b490c3531
Peter Oomen <petertheoomen@gmail.com>
2018-12-10 20:28:08 +1300

    Fixed bug with scrolling being impossible on certain screen sizes

--- static/js/header-bar.js
+++ static/js/header-bar.js
@@ -3,20 +3,22 @@ var headerCollapsed = false;
 document.addEventListener("DOMContentLoaded", function(event) { 
   var header = document.getElementById('header-bar');
   var content = document.getElementById('content');
-  content.addEventListener('scroll', function() {
+
+  var onScroll = function(e) {
     if (headerCollapsed) {
-      if (content.scrollTop == 0) {
+      if (content.scrollTop == 0 && e.deltaY < 0) {        
         header.style.top = '0'; 
         content.style.top = '156px'; 
-        content.scrollTop = 0;
         headerCollapsed = !headerCollapsed;
       }
     } else {
-      if (content.scrollTop > 0) {
+      if (e.deltaY > 0) {
         header.style.top = '-108px';
         content.style.top = '48px';
         headerCollapsed = !headerCollapsed;
       }
     }
-  });
+  };
+
+  content.addEventListener('wheel', onScroll);
 });
